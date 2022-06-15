ddfe112d9c0f84fb3b405d2bc2b11893b2184be4
George MacKerron <george@mackerron.co.uk>
2022-06-15 13:42:07 +0100

    CSS fun

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import m from 'mithril';
 
 interface OthelloAttrs {
   boardStr: string;
+  lastPieceStr: string;
   turnStr: string;
 }
 
@@ -15,13 +16,45 @@ interface Position {  // can also represent a vector movement
 type Board = (0 | 1 | typeof x)[];
 
 
+const
+  size = 8,
+  x = 2,  // signifies blank square: **MUST BE 2** because we use base 3 to serialize
+  initialBoard: Board = [
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+    x, x, x, 1, 0, x, x, x,
+    x, x, x, 0, 1, x, x, x,
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+  ],
+  codeChars = '234567bcdfghjkmnpqrstvwxyz-',
+  initialBoardStr = stringFromBoard(initialBoard),
+  routeTemplate = '/othello/:boardStr/:lastPieceStr/:turnStr',
+  defaultRoute = `/othello/${initialBoardStr}/-/0`,
+  players = [
+    { name: 'Black', colour: 'black' },
+    { name: 'White', colour: 'white' },
+  ],
+  directions: Position[] = [
+    { rightwards: 1, downwards: 0 }, // left -> right
+    { rightwards: 1, downwards: 1 }, // top-left -> bottom-right
+    { rightwards: 0, downwards: 1 }, // top -> bottom
+    { rightwards: -1, downwards: 1 }, // top-right -> bottom-left
+    { rightwards: -1, downwards: 0 }, // right -> left
+    { rightwards: -1, downwards: -1 }, // bottom-right -> top-left
+    { rightwards: 0, downwards: -1 }, // bottom -> top 
+    { rightwards: 1, downwards: -1 }, // bottom-left -> top-right
+  ];
+
+
 function stringFromBoard(board: Board) {
-  return board.join('').match(/.{2}/g)!.map(x => parseInt(x, 3)).join('');
+  return (board.join('') + '22').match(/.{1,3}/g)!.map(x => codeChars.charAt(parseInt(x, 3))).join('');
 }
 
 function boardFromString(s: string) {
-  // the `+ 9` adds a 1 at the front of the number; we slice this off to guarantee 2 digits
-  return s.split('').flatMap(x => Number(x + 9).toString(3).slice(-2).split('').map(Number)) as Board;
+  return s.split('').flatMap(x => (27 + codeChars.indexOf(x)).toString(3).slice(-3).split('').map(Number)).slice(0, 64) as Board;
 }
 
 function positionFromPieceIndex(pieceIndex: number): Position | undefined {
@@ -72,65 +105,35 @@ function flipPiecesByDirections(board: Board, position: Position, pieceCounts: n
 function playAtPieceIndex(board: Board, pieceIndex: number, player: 0 | 1) {
   const currentPiece = board[pieceIndex];
 
-  if (currentPiece !== x) {
-    alert(`Sorry, you can't go there: there's a piece there already`);
-    return;
-  }
+  if (currentPiece !== x) return;
 
   const
     position = positionFromPieceIndex(pieceIndex)!,
     flippablesByDirection = flippableOpponentPiecesByDirection(board, position, player),
     flippablesCount = flippablesByDirection.reduce((memo, n) => memo + n);
 
-  if (flippablesCount === 0) {
-    alert(`Sorry, you can't go there: you must flip at least one of your opponent's pieces every turn`);
-    return;
-  }
+  if (flippablesCount === 0) return pieceIndex;
 
   const newBoard = [...board];
   newBoard[pieceIndex] = player;
   flipPiecesByDirections(newBoard, position, flippablesByDirection);
 
   const newPlayer = 1 - player as 0 | 1;
-  m.route.set('/othello/:boardStr/:turnStr', { boardStr: stringFromBoard(newBoard), turnStr: newPlayer });
+  m.route.set(routeTemplate, { boardStr: stringFromBoard(newBoard), lastPieceStr: pieceIndex, turnStr: newPlayer });
 }
 
-const
-  directions: Position[] = [
-    { rightwards: 1, downwards: 0 }, // left -> right
-    { rightwards: 1, downwards: 1 }, // top-left -> bottom-right
-    { rightwards: 0, downwards: 1 }, // top -> bottom
-    { rightwards: -1, downwards: 1 }, // top-right -> bottom-left
-    { rightwards: -1, downwards: 0 }, // right -> left
-    { rightwards: -1, downwards: -1 }, // bottom-right -> top-left
-    { rightwards: 0, downwards: -1 }, // bottom -> top 
-    { rightwards: 1, downwards: -1 }, // bottom-left -> top-right
-  ],
-  size = 8,
-  x = 2,  // signifies blank square: **MUST BE 2** because we use base 3 to serialize
-  initialBoard: Board = [
-    x, x, x, x, x, x, x, x,
-    x, x, x, x, x, x, x, x,
-    x, x, x, x, x, x, x, x,
-    x, x, x, 1, 0, x, x, x,
-    x, x, x, 0, 1, x, x, x,
-    x, x, x, x, x, x, x, x,
-    x, x, x, x, x, x, x, x,
-    x, x, x, x, x, x, x, x,
-  ],
-  initialBoardStr = stringFromBoard(initialBoard),
-  players = [
-    { name: 'Black', colour: 'black' },
-    { name: 'White', colour: 'white' },
-  ];
-
 export function Othello() {
+  let errorIndex: number | undefined;
+
   return {
     view: (vnode: m.Vnode<OthelloAttrs>) => {
       const
-        { boardStr, turnStr } = vnode.attrs,
+        { boardStr, turnStr, lastPieceStr } = vnode.attrs,
         board = boardFromString(boardStr),
-        turnForPlayer = Number(turnStr) as 0 | 1;
+        turnForPlayer = Number(turnStr) as 0 | 1,
+        lastPieceIndex = lastPieceStr === '-' ? undefined : Number(lastPieceStr),
+        lastPiecePosition = positionFromPieceIndex(lastPieceIndex ?? -1),
+        piecesPerPlayer = board.reduce((memo, piece) => { memo[piece] += 1; return memo; }, [0, 0, 0]);
 
       return m('.game',
         players.map((player, playerIndex) => m('.player',
@@ -145,8 +148,7 @@ export function Othello() {
           m('.piece', {
             style: {
               display: 'inline-block',
-              width: '16px',
-              height: '16px',
+              width: '16px', height: '16px',
               borderRadius: '16px',
               border: '1px solid #251',
               margin: '1px 10px 5px',
@@ -155,49 +157,67 @@ export function Othello() {
               background: players[playerIndex].colour,
             }
           }),
-          player.name
+          player.name, ' — ', piecesPerPlayer[playerIndex]
         )),
         m('.board',
           {
             style: {
               background: '#372',
-              width: '736px',
-              height: '736px',
+              width: '720px', height: '720px',
               borderRadius: '40px',
               margin: '10px 0',
-              padding: '10px',
+              padding: '20px',
             }
           },
-          board.map((playerIndex, pieceIndex) => m('div',
-            {
-              style: {
-                width: '80px',
-                height: '80px',
-                float: 'left',
-                lineHeight: '0px',
-                margin: '5px',
-                borderRadius: '82px',
-                border: '1px solid #251',
+          board.map((playerIndex, pieceIndex) => {
+            const
+              commonPieceSizeStyle = { width: '80px', height: '80px', borderRadius: '80px' },
+              piecePosition = positionFromPieceIndex(pieceIndex)!,
+              distanceFromLastPlayed = lastPiecePosition === undefined ? 1 : Math.sqrt(
+                Math.pow(piecePosition.rightwards - lastPiecePosition.rightwards, 2) +
+                Math.pow(piecePosition.downwards - lastPiecePosition.downwards, 2)
+              ),
+              commonPieceStyle = {
+                ...commonPieceSizeStyle, position: 'absolute', backfaceVisibility: 'hidden',
+                top: playerIndex === x ? '-20px' : '0',
+                transition: pieceIndex === lastPieceIndex ? 'top .25s' : `transform ${.25 * distanceFromLastPlayed}s`
+              };
+
+            return m('div',
+              {
+                style: {
+                  ...commonPieceSizeStyle,
+                  position: 'relative',
+                  float: 'left',
+                  margin: '5px',
+                  background: pieceIndex === errorIndex ? 'rgba(255, 0, 0, .5)' :
+                    turnForPlayer === 0 ? 'rgba(0, 0, 0, .05)' : 'rgba(255, 255, 255, .05)',
+                  boxShadow: pieceIndex === lastPieceIndex ? '0 0 12px #ff0' : 'none',
+                  transition: 'box-shadow .25s .25s, background .5s',
+                },
+                onclick: () => errorIndex = playAtPieceIndex(board, pieceIndex, turnForPlayer)
               },
-              onclick: () => playAtPieceIndex(board, pieceIndex, turnForPlayer)
-            },
-            m('.piece', {
-              style: {
-                width: '80px',
-                height: '80px',
-                borderRadius: '80px',
-                transition: 'transform .4s linear',
-                transform: playerIndex === 0 ? 'rotateY(0deg)' : playerIndex === 1 ? 'rotateY(180deg)' : 'rotateY(-90deg)',
-                background: playerIndex !== x ? players[playerIndex].colour : 'transparent',
-              }
-            })
-          ))
-        )
+              m('.piece0', {
+                style: {
+                  ...commonPieceStyle,
+                  transform: playerIndex === 0 ? 'rotateY(0deg)' : playerIndex === 1 ? 'rotateY(180deg)' : 'rotateY(90deg)',
+                  background: playerIndex !== x ? players[0].colour : 'transparent',
+                }
+              }),
+              m('.piece1', {
+                style: {
+                  ...commonPieceStyle,
+                  transform: playerIndex === 0 ? 'rotateY(180deg)' : playerIndex === 1 ? 'rotateY(0deg)' : 'rotateY(90deg)',
+                  background: playerIndex !== x ? players[1].colour : 'transparent',
+                }
+              })
+            )
+          })
+        ),
+        m(m.route.Link, { selector: 'button', href: defaultRoute }, 'Start again')
       )
     }
   };
 }
 
-m.route(document.body, `/othello/${initialBoardStr}/0`, {
-  '/othello/:boardStr/:turnStr': Othello,
-});
+m.route(document.body, defaultRoute, { [routeTemplate]: Othello });
